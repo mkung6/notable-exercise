c4f8124d60a72e00f773b831f16a988489ef9a6b
mkung <tarrant6@gmail.com>
2019-06-28 15:56:49 -0700

    fixed code

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import Physicians from './components/Physicians';
 import Appointments from './components/Appointments';
 
 class App extends Component {
-    getAppointments = (id) => {
-        return 1;
-    }
 
     render() {
         const getPhysicians = this.props.allPhysiciansQuery.physicians || [];
 
         return (
             <div>
+            PHYSICIANS
             {getPhysicians.map(physicians => (
                 <Physicians
                     id={physicians.id}
                     firstName={physicians.firstName}
                     lastName={physicians.lastName}
-                    onClick={this.getAppointments(physicians.id)}
                 />
             ))}
             </div>
@@ -51,5 +48,5 @@ const APPOINTMENTS_QUERY = gql`
 
 export default compose(
     graphql(ALL_PHYSICIANS_QUERY, { name: 'allPhysiciansQuery' }),
-    graphql(APPOINTMENTS_QUERY, { name: 'getAppointmentsQuery' }),
+    graphql(APPOINTMENTS_QUERY, { name: 'getAppointmentsQuery' })
 )(App);
